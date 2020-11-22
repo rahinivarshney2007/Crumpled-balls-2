1804a44ef4a72a8680e719d52da4797c41d2cf4e
rahinivarshney2007 <73424396+rahinivarshney2007@users.noreply.github.com>
2020-11-22 12:36:51 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball{
+    constructor(x,y){
+    var options = {
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2,
+    }
+this.image=loadImage("sprites/paper.png")
+this.body=Bodies.circle(x,y,40,options)
+World.add(world,this.body)
+
+
+}
+
+fetch(){
+var pos=this.body.position
+
+fill("green")
+imageMode(RADIUS)
+image(this.image,pos.x,pos.y,40,40)
+//ellipse(pos.x,pos.y,20)
+
+}
+}
+
+//display an image 
+//image(location,x,y,width,height)
